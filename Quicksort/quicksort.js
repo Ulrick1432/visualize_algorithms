7fe269131a49df14754ed17f6756ef446a0c82b1
Ulrick Preben Hansen <ulrick.preben.hansen@live.dk>
2025-02-02 13:29:27 +0100

    default styling added

--- Quicksort/quicksort.js
+++ Quicksort/quicksort.js
@@ -1,4 +1,16 @@
-const swap = require('./swap');
+//Docs https://www.geeksforgeeks.org/quick-sort-algorithm/?ref=shm
+
+// Function to add elements to the DOM
+const addElement = (value) => {
+  const container = document.querySelector(".quick-sort");
+  const element = document.createElement("div");
+  element.classList.add("element", `element-value-${value}`);
+  element.textContent = value;
+  container.appendChild(element);
+};
+
+  // Delay function
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const swap = (arr, indexOne, indexTwo) => {
   const temp = arr[indexTwo];
